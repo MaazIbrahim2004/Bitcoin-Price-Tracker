805f4df9c5960e77bfbaac29d8a8d08f6a87b166
Maaz Ibrahim <maazibrahim2004@gmail.com>
2023-07-15 02:36:33 -0400

    Update btc.py

--- btc.py
+++ btc.py
@@ -2,7 +2,29 @@ import requests
 import tkinter as tk #GUI 
 from datetime import datetime
 
+def trackBitcoin():
+    url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR"
+    response = requests.get(url).json()
+    print(response)
+
 
 canvas = tk.Tk()
 canvas.geometry("500x500")
 canvas.title("Bitcoin Price Index")
+
+f1 = ("poppins", 24, "bold")
+f2 = ("poppins", 22, "bold")
+f3 = ("poppins", 18, "normal")
+
+label = tk.Label(canvas, text = "Bitcoin Price Index", font = f1)
+label.pack(pady = 20)
+
+labelPrice = tk.Label(canvas, font = f2)
+labelPrice.pack(pady = 20)
+
+labelTime = tk.Label(canvas, font = f3) #time that the price was last updated
+labelTime.pack(pady = 20)
+
+trackBitcoin()
+
+canvas.mainloop()
