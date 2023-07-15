9fdb4394bfbc912b7ac94266dfbfefe788fb2a1c
Maaz Ibrahim <maazibrahim2004@gmail.com>
2023-07-15 03:22:45 -0400

    Update btc.py

--- btc.py
+++ btc.py
@@ -1,21 +1,24 @@
-import requests 
-import tkinter as tk #GUI 
+import requests
+import tkinter as tk  # GUI
 from datetime import datetime
 
-def trackBitcoin():
+
+# BTC API Used: https://min-api.cryptocompare.com/documentation?key=Price&cat=multipleSymbolsFullPriceEndpoint
+def track_bitcoin():
     url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR"
     response = requests.get(url).json()
     price_usd = response["USD"]
     price_jpy = response["JPY"]
     price_eur = response["EUR"]
-    time = datetime.now().strftime("%I:%M:%S %p")  # Changed to 12-hour format with AM/PM
+    time = datetime.now().strftime("%I:%M:%S %p")  # time that the price was last updated
 
-    labelPriceUSD.config(text = "USD: " + str(price_usd) + "$")
-    labelPriceJPY.config(text = "JPY: " + str(price_jpy) + "¥")
-    labelPriceEUR.config(text = "EUR: " + str(price_eur) + "€")
-    labelTime.config(text = "Updated at: " + time)
+    labelPriceUSD.config(text="USD: " + str(price_usd) + "$")
+    labelPriceJPY.config(text="JPY: " + str(price_jpy) + "¥")
+    labelPriceEUR.config(text="EUR: " + str(price_eur) + "€")
+    labelTime.config(text="Updated at: " + time)
 
-    canvas.after(1000, trackBitcoin) #refreshes the price every 1000 milliseconds
+    canvas.after(1000, track_bitcoin)  # refreshes the price every 1000 milliseconds
+
 
 canvas = tk.Tk()
 canvas.geometry("500x500")
@@ -25,21 +28,21 @@ f1 = ("poppins", 24, "bold")
 f2 = ("poppins", 22, "bold")
 f3 = ("poppins", 18, "normal")
 
-label = tk.Label(canvas, text = "Bitcoin Price Index", font = f1)
-label.pack(pady = 20)
+label = tk.Label(canvas, text="Bitcoin Price Index", font=f1)
+label.pack(pady=20)
 
-labelPriceUSD = tk.Label(canvas, font = f2)
-labelPriceUSD.pack(pady = 20)
+labelPriceUSD = tk.Label(canvas, font=f2)
+labelPriceUSD.pack(pady=20)
 
-labelPriceJPY = tk.Label(canvas, font = f2)
-labelPriceJPY.pack(pady = 20)
+labelPriceJPY = tk.Label(canvas, font=f2)
+labelPriceJPY.pack(pady=20)
 
-labelPriceEUR = tk.Label(canvas, font = f2)
-labelPriceEUR.pack(pady = 20)
+labelPriceEUR = tk.Label(canvas, font=f2)
+labelPriceEUR.pack(pady=20)
 
-labelTime = tk.Label(canvas, font = f3) #time that the price was last updated
-labelTime.pack(pady = 20)
+labelTime = tk.Label(canvas, font=f3)  
+labelTime.pack(pady=20)
 
-trackBitcoin()
+track_bitcoin()
 
 canvas.mainloop()
